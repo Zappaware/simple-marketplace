7482de8cc39ea0aa60a6b8b3048b2b323cbca63e
Sergio Cervantes <sergio_germanc@hotmail.com>
2025-04-15 12:55:34 -0600

    Add contact view and url
    Add contact view html and url to the project, also changing the "welcome
    title"

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from core.views import index
+from core.views import contact, index
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("", index, name="index"),
+    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
 ]

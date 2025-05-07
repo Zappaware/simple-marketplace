4874b3d1166eaba1d9e4a0ffdf77c91a50a3e384
Sergio Cervantes <sergio_germanc@hotmail.com>
2025-05-07 11:28:45 -0600

    Add Signup and login forms
    Adding the forms, authentication, redirect and configurations in
    settings.py

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from core.views import contact, index
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", include("core.urls")),
     path("items/", include("item.urls")),
-    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
